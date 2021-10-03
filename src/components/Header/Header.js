b93348f32ca477466c390889c519a474c22f0421
shudipto <shudipta15-11424@diu.edu.bd>
2021-10-03 13:17:09 +0600

    link tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css';
+
 
 const Header = () => {
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
             <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
+                <Link href="/shop">Shop</Link>
+                <Link href="/review">Order Review</Link>
+                <Link href="/inventory">Manage Inventory</Link>
             </nav>
         </div>
     );
